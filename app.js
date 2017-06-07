df65430a35a45d4e7952ca668d0b756207f2599f
Kai Hoffman <hoffm154@purdue.edu>
2017-06-07 16:09:52 -0400

    Make all buttons work I hope

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const app = {
     const star = document.createTextNode('\u2606')
     btn.id = 'star:' + name
     btn.className = 'star'
+    btn.value = false
     btn.addEventListener('click', this.faveFlick.bind(this))
     btn.appendChild(star)
     return btn
@@ -25,6 +26,7 @@ const app = {
     const up = document.createTextNode('\u25b2')
     btn.id = 'up:' + name
     btn.className = 'up'
+    btn.addEventListener('click', this.moveFlick.bind(this))
     btn.appendChild(up)
     return btn
   },
@@ -34,6 +36,7 @@ const app = {
     const down = document.createTextNode('\u25bc')
     btn.id = 'down:' + name
     btn.className = 'down'
+    btn.addEventListener('click', this.moveFlick.bind(this))
     btn.appendChild(down)
     return btn
   },
@@ -43,6 +46,7 @@ const app = {
     const x = document.createTextNode('\u2612')
     btn.id = 'x:' + name
     btn.className = 'x'
+    btn.addEventListener('click', this.deleteFlick.bind(this))
     btn.appendChild(x)
     return btn
   },
@@ -55,6 +59,7 @@ const app = {
     const downBtn = this.makeDownBtn(name)
     const xBtn = this.makeXBtn(name)
     li.textContent = name
+    li.id = name
     li.appendChild(starBtn)
     li.appendChild(xBtn)
     li.appendChild(downBtn)
@@ -78,7 +83,42 @@ const app = {
 
   faveFlick(ev) {
     ev.preventDefault()
-    console.log(ev.target)
+    const btn = ev.target
+    const movieName = btn.id.substring(btn.id.indexOf(':') + 1)
+    const li = document.getElementById(movieName)
+    if (btn.value === 'false') {
+      li.style.backgroundColor = 'yellow'
+      btn.value = true
+    }
+    else if (btn.value === 'true') {
+      li.style.backgroundColor = '#F5F5DC'
+      btn.value = false
+    }
+  },
+
+  moveFlick(ev) {
+    ev.preventDefault()
+    const btn = ev.target
+    const upOrDown = btn.id.substring(0, btn.id.indexOf(':'))
+    const movieName = btn.id.substring(btn.id.indexOf(':') + 1)
+    const thisItem = btn.parentElement
+    const nextItem = thisItem.nextSibling
+    const prevItem = thisItem.previousSibling
+    if (upOrDown === 'up' && prevItem != null) {
+      this.list.insertBefore(thisItem, prevItem)
+      // parentNode.insertBefore(childToMove, childToPrecede)
+    }
+    else if (upOrDown === 'down' && nextItem != null) {
+      this.list.insertBefore(nextItem, thisItem)
+    }
+  },
+
+  deleteFlick(ev) {
+    ev.preventDefault()
+    const btn = ev.target
+    const movieName = btn.id.substring(btn.id.indexOf(':') + 1)
+    const li = document.getElementById(movieName)
+    li.remove()
   },
 }
 
